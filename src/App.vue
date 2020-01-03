8600625bb8bded386049caa52f6794419ecb11a1
Mateusz <mateusz.grochowski1992@gmail.com>
2020-01-03 02:08:40 +0100

    init project with vue cli

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+  </div></template>
+
+<style lang="scss">
+*, *:after, *:before {
+    box-sizing: border-box;
+}
+html ,
+body {
+  padding:0px; 
+  margin:0px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: rgb(30, 29, 42);
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
