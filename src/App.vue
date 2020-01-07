b6005ac23f871009bfbeba264a52252134375975
mrMatie <mateusz.grochowski1992@gmail.com>
2020-01-07 01:49:25 +0100

    set up main template

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,60 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <router-link to="/">Home</router-link>|
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view />
     </div>
-    <router-view/>
-  </div></template>
+</template>
 
 <style lang="scss">
-*, *:after, *:before {
+*,
+*:after,
+*:before {
     box-sizing: border-box;
 }
-html ,
-body {
-  padding:0px; 
-  margin:0px;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html, body {
   min-height: 100%;
-  display: flex;
-  flex-direction: column;
-  background-color: rgb(30, 29, 42);
+  margin: 0;
+
+    padding: 0px;
+}
+// html,
+// body {
+//     padding: 0px;
+//     margin: 0px;
+// }
+// html{
+//   height: 100%;
+// }
+// body {
+//   min-height: 100%;
+// }
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    //min-height: 100%;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background-color: rgb(30, 29, 42);
+		position:relative
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
